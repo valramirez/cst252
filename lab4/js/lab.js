a108fbdc0b47973e1bf3b4a514bca3db054d8668
valramirez <valeramirez@csumb.edu>
2020-09-16 00:35:21 -0700

    make lab 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,1 +1,6 @@
-//pee pee poo poo check
+//Gather a collection of images
+  // Save image list
+    //Link images to one spot
+      //Assign each image a number
+        //Generate math problem to make order random
+          //Click refresh and enjoy cat
---
+++ lab5/js/lab.js
@@ -0,0 +1,15 @@
+//  Define Variables
+  make = "Suzuki";
+  model = "Forenza";
+  color = "Brown";
+  year = "2007";
+
+// calculate
+  age = 2019 - year;
+
+// output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
