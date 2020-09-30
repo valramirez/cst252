4abcd074d1f506435807651a5a076a5950fcea88
valramirez <valeramirez@csumb.edu>
2020-09-30 01:35:53 -0700

    made lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,29 @@
+/*
+ * Author: Val Ramirez <valeramirez@csumb.edu>
+ * Created: 30 September
+ * License: Public Domain
+ */
+
+
+  //here's my attempt to figure this output
+      // welcome aboard :)
+
+
+//Find the target element
+      var outputEl =
+        document.createElement("p");
+        document.body.appendChild(outputEl);
+
+//Set the text of the element
+    var new1El =
+      document.createElement("p");
+
+    new1El.innerHTML = "When all of this is over, I want to go on a picninc date with my partner";
+    document.body.appendChild(new1El);
+
+//Create another second element
+    var new2El =
+      document.createElement ("p2");
+
+    new2El.innerHTML = "And hold hands and watch the sun setting together";
+    document.body.appendChild(new2El);
